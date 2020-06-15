52b80ff5d7ae13879b9840a1497b5a2858bb494b
theomgxz <sg28r8@gmail.com>
2020-06-15 11:13:40 -0400

    backend boilerplate setup

---
+++ app.js
@@ -0,0 +1,45 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', indexRouter);
+//// error handling 
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  } else {
+    next()
+  }
+});
+app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || "Internal server error.");
+  });
+};
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
