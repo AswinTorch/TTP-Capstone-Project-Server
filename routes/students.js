1c1db4272ce71999c4497c702d73e45f1cbd3ee7
theomgxz <sg28r8@gmail.com>
2020-06-16 10:20:38 -0400

    added ep for remove and swap courses for students

--- routes/students.js
+++ routes/students.js
@@ -1,31 +1,36 @@
 var express = require("express");
 var router = express.Router();
-var firebase = require("firebase")
-var db = require("./db"); 
+var firebase = require("firebase");
+var db = require("./db");
 
 router.get("/id/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
-    var return_val = await db.collection("Students").doc(id).get().then((doc) => {
-      if (doc.exists) {
-        return doc.data();
-      } else {
-        console.log("no such data found")
-      }
-    }).catch((err) => {
-      console.log("no such data found on db")
-    });
-    res.status(200).json(return_val)
+    var return_val = await db
+      .collection("Students")
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return doc.data();
+        } else {
+          console.log("no such data found");
+        }
+      })
+      .catch((err) => {
+        console.log("no such data found on db");
+      });
+    res.status(200).json(return_val);
   } catch (err) {
     next(err);
   }
-})
-//POST ON EVERY 
+});
+//POST ON EVERY
 router.post("/", async (req, res) => {
   const { UID, firstName, lastName, emailAddress } = req.body;
   console.log(UID, firstName);
   try {
-    console.log(req.body["uid"])
+    console.log(req.body["uid"]);
     let return_id = await db
       .collection("Students")
       .add({
@@ -43,31 +48,35 @@ router.post("/", async (req, res) => {
       .catch((err) => {
         console.log("Couldn't post for some odd reason");
       });
-    res.status(201).send(return_id)
+    res.status(201).send(return_id);
   } catch (err) {
-    res.status(400).send(`${err} :::Couldn't add for some ungodly reason`)
+    res.status(400).send(`${err} :::Couldn't add for some ungodly reason`);
   }
-})
+});
 //Checks if the student exists on the database or not and return [false] if not found | [[true,Student.id]] if found.
 router.get("/exists/:uid_o", async (req, res, next) => {
   // try to get students object from database
-  const {uid_o} = req.params;
+  const { uid_o } = req.params;
   // console.log(name, email);
   try {
-    var return_bool = await db.collection("Students").where("uid","==",uid_o).get().then(snapShot => {
-      var return_val = [];
-      if (snapShot.empty) {
-        
-        return_val.push(false);
-      } else {
-        snapShot.forEach(doc => {
-          return_val.push([true, doc.id]);
-        })
-      }
-      return return_val;
-    }).catch(err => {
-      console.log("Error on retrieval",err)
-    })
+    var return_bool = await db
+      .collection("Students")
+      .where("uid", "==", uid_o)
+      .get()
+      .then((snapShot) => {
+        var return_val = [];
+        if (snapShot.empty) {
+          return_val.push(false);
+        } else {
+          snapShot.forEach((doc) => {
+            return_val.push([true, doc.id]);
+          });
+        }
+        return return_val;
+      })
+      .catch((err) => {
+        console.log("Error on retrieval", err);
+      });
     res.status(200).send(return_bool);
   } catch (err) {
     next(err);
@@ -75,24 +84,59 @@ router.get("/exists/:uid_o", async (req, res, next) => {
 });
 //insert classes enrolled
 //takes in the student id as parameter
-//& courseID as json object
+//& course_id as json object
 router.put("/addCourse/:id", async (req, res, next) => {
   const { id } = req.params;
-  const { courseId } = req.body;  
+  const { course_id } = req.body;
   try {
-    var current_student =await db.collection("Students").doc(id);
+    var current_student = await db.collection("Students").doc(id);
     current_student.update({
-      enrolled_classes: firebase.firestore.FieldValue.arrayUnion(courseId)
-    })
+      enrolled_classes: firebase.firestore.FieldValue.arrayUnion(course_id),
+    });
     res.status(201).send("sucesss");
   } catch (err) {
-    next(err)
+    next(err);
   }
-}) 
-  
-
+});
+//Remove Course
+router.put("/removeCourse/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { course_id } = req.body;
+  try {
+    var current_student = await db.collection("Students").doc(id);
+    current_student.update({
+      enrolled_classes: firebase.firestore.FieldValue.arrayRemove(course_id),
+    });
+    res.status(201).send("success");
+  } catch (err) {
+    next(err);
+  }
+});
+//Swap Course
+//req body take to arguments
+//prev course id and the the new id
+router.put("/swapCourse/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { prev_course_id, new_course_id } = req.body;
+  try {
+    var current_student = await db.collection("Students").doc(id);
+    current_student.update({
+      enrolled_classes: firebase.firestore.FieldValue.arrayRemove(
+        prev_course_id
+      ),
+    });
+    current_student.update({
+      enrolled_classes: firebase.firestore.FieldValue.arrayUnion(
+        new_course_id
+      ),
+    });
+    res.status(201).send("success")
+  } catch (err) {
+    next(err);
+  }
+});
 
 async function get_course() {
-  return
+  return;
 }
 module.exports = router;
