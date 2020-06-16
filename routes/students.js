6a0b1bdb7e65f3c484427c177de065740d0c178d
theomgxz <sg28r8@gmail.com>
2020-06-16 18:02:07 -0400

    Fix to the GET op for student

--- routes/students.js
+++ routes/students.js
@@ -6,38 +6,43 @@ var db = require("./db");
 /**
  * GET student object from their id
  * /api/students/id/:id
- * 
+ *
  * /api/students/id/10 would return the student object with id 10, if it exists
- * 
+ *
  * Returns: the student object associated with the id
- * 
+ *
  * Return status:
  * 200 - OK
  * 404 - Not found
  */
-router.get("/id/:id", async (req, res, next) => 
-{
-    const { id } = req.params;
+router.get("/id/:id", async (req, res, next) => {
+  const { id } = req.params;
 
-    try 
-    {
-        var return_val = await db.collection("Students")
-        .doc(id).get()
-        .then((doc) => { return doc.data(); })
-        .catch((err) => console.error(err));
-
-        res.status(200).json(return_val);
-    } 
-    catch(err) 
-    {
-        res.status(404).send(`${err} :: Error getting a student`);
-    }
+  try {
+    var return_val = await db
+      .collection("Students")
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (!doc.exists) {
+          console.log("No such element");
+          res.status(404).send("error");
+        } else {
+          res.status(200).send(doc.data())
+        }
+      })
+      .catch((err) => {
+        res.status(err)
+      });
+  } catch (err) {
+    res.status(404).send(`${err} :: Error getting a student`);
+  }
 });
 
 /**
  * POST new student
  * /api/students/
- * 
+ *
  * Creates a new student object using the following information from the request body:
  * - uid
  * - displayName: first and last name
@@ -47,45 +52,47 @@ router.get("/id/:id", async (req, res, next) =>
  * - total_received: 0
  * - total_owed: 0
  * - enrolled_classes: []
- * 
+ *
  * Upon sign in, Firebase Authentication will generate a random uid for the new user.
  * After sign in, the signed-in user can be accessed by using the currentUser property:
- * 
+ *
  * let user = firebase.auth().currentUser;
- * 
+ *
  * Returns: the newly created UUID
- * 
+ *
  * Return status:
  * 201 - Created
  * 400 - Bad Request
  */
-router.post("/", async (req, res) => 
-{
-    const { uid, firstName, lastName, email } = req.body;
+router.post("/", async (req, res) => {
+  const { uid, firstName, lastName, email } = req.body;
 
-    try 
-    {
-        let return_id = await db.collection("Students")
-        .add(
-        {
-            uid: uid,
-            first_name: firstName,
-            last_name: lastName,
-            email: email,
-            total_credit: 0,
-            total_received: 0,
-            total_owed: 0,
-            enrolled_classes: [],
-        })
-        .then((ref) => { return ref.id; })
-        .catch((err) => console.error(err));
+  try {
+    let return_id = await db
+      .collection("Students")
+      .add({
+        uid: uid,
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        total_credit: 0,
+        total_received: 0,
+        total_owed: 0,
+        enrolled_classes: [],
+      })
+      .then((ref) => {
+        return ref.id;
+      })
+      .catch((err) => console.error(err));
 
-        res.status(201).send(return_id);
-    } 
-    catch(err) 
-    {
-        res.status(400).send(`${err} :: Error posting a new student. Be sure to include a uid, first_name, last_name, and email.`);
-    }
+    res.status(201).send(return_id);
+  } catch (err) {
+    res
+      .status(400)
+      .send(
+        `${err} :: Error posting a new student. Be sure to include a uid, first_name, last_name, and email.`
+      );
+  }
 });
 
 //insert classes enrolled
@@ -133,11 +140,9 @@ router.put("/swapCourse/:id", async (req, res, next) => {
       ),
     });
     current_student.update({
-      enrolled_classes: firebase.firestore.FieldValue.arrayUnion(
-        new_course_id
-      ),
+      enrolled_classes: firebase.firestore.FieldValue.arrayUnion(new_course_id),
     });
-    res.status(201).send("success")
+    res.status(201).send("success");
   } catch (err) {
     next(err);
   }
