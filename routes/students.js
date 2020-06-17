9810b392f11ebe519be1c0ec3478d8f2d3e0293c
Aswin Nair <aswintorch@gmail.com>
2020-06-17 16:49:08 -0400

    Changed class references to courses

--- routes/students.js
+++ routes/students.js
@@ -15,27 +15,23 @@ var db = require("./db");
  * 200 - OK
  * 404 - Not found
  */
-router.get("/id/:id", async (req, res, next) => 
-{
-    const { id } = req.params;
+router.get("/id/:id", async (req, res, next) => {
+  const { id } = req.params;
 
-    try 
-    {
-        var return_val = await db
-        .collection("Students")
-        .doc(id)
-        .get()
-        .then((doc) => 
-        {
-            if (!doc.exists) res.status(404).send("Error getting a student. Is the ID correct?");
-            else res.status(200).send(doc.data());
-        })
-        .catch((err) => console.error(err));
-    } 
-    catch(err) 
-    {
-        res.status(404).send(`${err} :: Error getting a student`);
-    }
+  try {
+    var return_val = await db
+      .collection("Students")
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (!doc.exists)
+          res.status(404).send("Error getting a student. Is the ID correct?");
+        else res.status(200).send(doc.data());
+      })
+      .catch((err) => console.error(err));
+  } catch (err) {
+    res.status(404).send(`${err} :: Error getting a student`);
+  }
 });
 
 /**
@@ -63,36 +59,37 @@ router.get("/id/:id", async (req, res, next) =>
  * 201 - Created
  * 400 - Bad Request
  */
-router.post("/", async (req, res) => 
-{
-    const { uid, firstName, lastName, email } = req.body;
+router.post("/", async (req, res) => {
+  const { uid, firstName, lastName, email } = req.body;
 
-    try 
-    {
-        // Creates a new student object and returns the uid
-        let return_id = await db
-        .collection("Students")
-        .doc(uid)
-        .set(
-        {
-            uid: uid,
-            first_name: firstName,
-            last_name: lastName,
-            email: email,
-            total_credit: 0,
-            total_received: 0,
-            total_owed: 0,
-            enrolled_courses: [],
-        })
-        .then(() => { return uid; })
-        .catch((err) => console.error(err));
+  try {
+    // Creates a new student object and returns the uid
+    let return_id = await db
+      .collection("Students")
+      .doc(uid)
+      .set({
+        uid: uid,
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        total_credit: 0,
+        total_received: 0,
+        total_owed: 0,
+        enrolled_courses: [],
+      })
+      .then(() => {
+        return uid;
+      })
+      .catch((err) => console.error(err));
 
-        res.status(201).send(return_id);
-    } 
-    catch (err) 
-    {
-        res.status(400).send(`${err} :: Error posting new student. Be sure to include a uid, first_name, last_name, and email.`);
-    }
+    res.status(201).send(return_id);
+  } catch (err) {
+    res
+      .status(400)
+      .send(
+        `${err} :: Error posting new student. Be sure to include a uid, first_name, last_name, and email.`
+      );
+  }
 });
 
 //insert classes enrolled
@@ -104,7 +101,7 @@ router.put("/addCourse/:id", async (req, res, next) => {
   try {
     var current_student = await db.collection("Students").doc(id);
     current_student.update({
-      enrolled_classes: firebase.firestore.FieldValue.arrayUnion(course_id),
+      enrolled_courses: firebase.firestore.FieldValue.arrayUnion(course_id),
     });
     res.status(201).send("sucesss");
   } catch (err) {
@@ -118,7 +115,7 @@ router.put("/removeCourse/:id", async (req, res, next) => {
   try {
     var current_student = await db.collection("Students").doc(id);
     current_student.update({
-      enrolled_classes: firebase.firestore.FieldValue.arrayRemove(course_id),
+      enrolled_courses: firebase.firestore.FieldValue.arrayRemove(course_id),
     });
     res.status(201).send("success");
   } catch (err) {
@@ -135,12 +132,12 @@ router.put("/swapCourse/:id", async (req, res, next) => {
   try {
     var current_student = await db.collection("Students").doc(id);
     current_student.update({
-      enrolled_classes: firebase.firestore.FieldValue.arrayRemove(
+      enrolled_courses: firebase.firestore.FieldValue.arrayRemove(
         prev_course_id
       ),
     });
     current_student.update({
-      enrolled_classes: firebase.firestore.FieldValue.arrayUnion(new_course_id),
+      enrolled_courses: firebase.firestore.FieldValue.arrayUnion(new_course_id),
     });
     res.status(201).send("success");
   } catch (err) {
